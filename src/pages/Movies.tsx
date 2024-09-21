9816a2310bb62f4f7cf28882f69adffef746ca67
alizaasifb <aliza.asif@mail.utoronto.ca>
2024-09-21 13:09:16 -0400

    set sidebar to not overflow when exceeding viewport height

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -11,7 +11,12 @@ function Movies() {
     return <>    
         <div className="container-fluid">
         <div className="row flex-nowrap ">
-            <div className="col-sm-3 sidebar col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sticky-top" style={{ height: '100vh' }}>
+            <div className="col-sm-3 sidebar col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sticky-top"  
+                style={{ 
+                    height: '100vh', 
+                    minHeight: '-webkit-fill-available', 
+                    overflowY: 'auto'
+                }}>
                 <Profile> Go to saved movies </Profile>
                 <MovieTypes moviesType={moviesType} setMoviesType={setMoviesType} />
             </div>
